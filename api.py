911d0874eefebbd3d02a321d3387b9f8a2201cb6
Florence Mnyazi <florencemnyazi@gmail.com>
2017-03-09 02:32:50 +0300

    console app with weather and wind conditions

---
+++ api.py
@@ -0,0 +1,16 @@
+import pyowm
+#imports pyowm library that contains weather data from Python
+print ("Welcome to our Weather Forecast Console App ")
+
+api_key = pyowm.OWM("833fb40410275d3ccdb66126e8f04e5a")
+#This creates a a variable api_key that contains the api_key to be used 
+forecast = api_key.weather_at_place("Nairobi,Kenya")
+#The variable forecast sets the place whose weather we wish to view
+w = forecast.get_weather()
+#This variable shortens the GET request function that will obtain the weather data
+wind = w.get_wind
+temperature = w.get_temperature('celsius')
+
+print (w)
+print (wind)
+print (temperature)
